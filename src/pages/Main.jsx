7d5b0bafbdc955d7f7e1b00fc03ff8137ae02fcc
Sola Jegede <sjegede1@gmail.com>
2023-07-26 13:48:38 -0500

    finished bitcoin api assignment

---
+++ src/pages/Main.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Main() {
+  return (
+    <>
+      <h1>Welcome to the CryptoDB</h1>
+      <h3>
+        Click on the currencies link to get access to the latest prices of each
+        cryptocurrencies
+      </h3>
+    </>
+  );
+}
+
+export default Main;
